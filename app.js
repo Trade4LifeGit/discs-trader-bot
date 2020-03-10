3a687f294fa8f6f2514a4627b525a12693d85da1
jonarizzz <jonarizzz@inbox.ru>
2020-03-10 16:22:53 +0300

    Submenu for login

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 const Telegraf = require('telegraf')
 const TelegrafInlineMenu = require('telegraf-inline-menu')
-const menu = new TelegrafInlineMenu('Main Menu')
 
-menu.simpleButton('Login', 'login click', {
+const menu = new TelegrafInlineMenu('Ну здравствуйте, тут у нас главное меню:)')
+
+menu.simpleButton('Логин', 'login click', {
   doFunc: async ctx => ctx.answerCbQuery('you clicked login')
 })
 
-menu.simpleButton('Register', 'register click', {
+menu.simpleButton('Регистрация', 'register click', {
   doFunc: async ctx => ctx.answerCbQuery('you clicked registration')
 })
 
 menu.setCommand('start')
 
+
+const loginMenu = new TelegrafInlineMenu('Меню входа')
+
+loginMenu.simpleButton('Логин', 'login click', {
+  doFunc: async ctx => ctx.answerCbQuery('you clicked login')
+})
+
+menu.submenu('Login menu', 'login', loginMenu)
+
+
 const bot = new Telegraf('960038286:AAGZZHaFHCj1VCxvkBhGTLAYYWxd7DT7JNk')
 
 bot.use((ctx, next) => {
   if (ctx.callbackQuery) {
-    //   console.log('another callbackQuery happened', ctx.callbackQuery.data.length, ctx.callbackQuery.data)
     console.log(ctx.callbackQuery.data)
   }
   return next()
