dfd52a2ddc07bbe3d01f7383bdcd0f49dde90561
jonarizzz <jonarizzz@inbox.ru>
2020-06-09 18:22:01 +0300

    Test menus

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const Telegraf = require('telegraf')
 const TelegrafInlineMenu = require('telegraf-inline-menu')
-const session = require('telegraf/session')
 
-
-const bot = new Telegraf('960038286:AAGZZHaFHCj1VCxvkBhGTLAYYWxd7DT7JNk')
+const bot = new Telegraf('960038286:AAGZZHaFHCj1VCxvkBhGTLAYYWxd7DT7JNk');
+bot.use(Telegraf.log());
 
 bot.catch(error => {
   console.log('telegraf error', error.response, error.parameters, error.on || error)
 })
 
 bot.start((ctx) => {
-  console.log(ctx.message);
   ctx.reply('Здарова, ' + ctx.from.first_name + ' ' + (ctx.from.last_name ? ctx.from.last_name : '') + '!\n' +
   'Тут такое дело: на данный момент бот находится в разработке и его функции ограничены, \n' +
   'но несмотря на это некоторые функции можно опробовать уже сейчас: \n' +
@@ -23,13 +21,37 @@ bot.command('personal', (ctx) => {
   ctx.reply('Личный кабинет ещё не готов');
 });
 
-bot.command('menu', (ctx) => {
-  ctx.reply('Меню ещё не готово');
+const testMenu = new TelegrafInlineMenu(ctx => `Тестовая менюшка`);
+testMenu.setCommand('test');
+testMenu.simpleButton('Первая кнопка меню', 'a', {
+  doFunc: ctx => ctx.answerCbQuery('Нажата кнопка 1')
 });
+testMenu.simpleButton('Вторая кнопка меню', 'b', {
+  doFunc: ctx => ctx.answerCbQuery('Нажата кнопка 2'),
+  joinLastRow: true,
+});
+
+bot.use(testMenu.init());
+// bot.startPolling();
+
+
+const firstMenu = new TelegrafInlineMenu(ctx => `Здравствуй, уважаемый ${ctx.from.first_name + ' ' + (ctx.from.last_name ? ctx.from.last_name : '')}`);
+firstMenu.setCommand('menu');
+firstMenu.simpleButton('Продажа', 'a', {
+  doFunc: ctx => ctx.answerCbQuery('Нажата кнопка 1')
+});
+firstMenu.simpleButton('Покупка', 'b', {
+  doFunc: ctx => ctx.answerCbQuery('Нажата кнопка 2'),
+  joinLastRow: true,
+});
+
+bot.use(firstMenu.init());
+bot.startPolling();
+
 
 async function startup() {
   await bot.launch()
   console.log(new Date(), 'Bot started as', bot.options.username)
 }
 
-startup()
+startup();
