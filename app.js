dc0503c2a25faf4a3d4b44ff664c4a8f6a6962b1
jonarizzz <jonarizzz@inbox.ru>
2020-08-17 22:45:16 +0300

    Issue with changing photo and name of the game

--- app.js
+++ app.js
@@ -1,49 +1,54 @@
 const Telegraf = require('telegraf')
 const TelegrafInlineMenu = require('telegraf-inline-menu')
+const {mockedGames, TELEGRAM_BOT_KEY} = require('./constants.js')
 
-const bot = new Telegraf('960038286:AAGZZHaFHCj1VCxvkBhGTLAYYWxd7DT7JNk');
-bot.use(Telegraf.log());
+const bot = new Telegraf(TELEGRAM_BOT_KEY);
+// bot.use(Telegraf.log());
 
 bot.catch(error => {
   console.log('telegraf error', error.response, error.parameters, error.on || error)
-})
-
-bot.start((ctx) => {
-  ctx.reply('Здарова, ' + ctx.from.first_name + ' ' + (ctx.from.last_name ? ctx.from.last_name : '') + '!\n' +
-  'Тут такое дело: на данный момент бот находится в разработке и его функции ограничены, \n' +
-  'но несмотря на это некоторые функции можно опробовать уже сейчас: \n' +
-  '/personal \n'+
-  '/menu \n' +
-  '/test');
 });
 
-bot.command('personal', (ctx) => {
-  ctx.reply('Личный кабинет ещё не готов');
-});
-
-const testMenu = new TelegrafInlineMenu(ctx => `Тестовая менюшка`);
-testMenu.setCommand('test');
-testMenu.simpleButton('Первая кнопка меню', 'a', {
-  doFunc: ctx => ctx.answerCbQuery('Нажата кнопка 1')
-});
-testMenu.simpleButton('Вторая кнопка меню', 'b', {
-  doFunc: ctx => ctx.answerCbQuery('Нажата кнопка 2'),
-  joinLastRow: true,
-});
-
-bot.use(testMenu.init());
-// bot.startPolling();
-
-
-const firstMenu = new TelegrafInlineMenu(ctx => `Здравствуй, уважаемый ${ctx.from.first_name + ' ' + (ctx.from.last_name ? ctx.from.last_name : '')}`);
-firstMenu.setCommand('menu');
+const firstMenu = new TelegrafInlineMenu(ctx => `Здравствуй, ${ctx.from.first_name + ' ' + (ctx.from.last_name ? ctx.from.last_name : '')}!`);
+firstMenu.setCommand('start');
 firstMenu.simpleButton('Продажа', 'a', {
-  doFunc: ctx => ctx.answerCbQuery('Нажата кнопка 1')
+  doFunc: ctx => ctx.answerCbQuery('Поздравляю! Вы ищите чего бы продать!')
 });
 firstMenu.simpleButton('Покупка', 'b', {
-  doFunc: ctx => ctx.answerCbQuery('Нажата кнопка 2'),
+  doFunc: ctx => ctx.answerCbQuery('Поздравляю! Вы ищите чего бы купить!'),
   joinLastRow: true,
 });
+firstMenu.simpleButton('Мои объявления', 'c', {
+  doFunc: ctx => ctx.answerCbQuery('Поздравляю! Вы решили посмотреть на свои лоты!')
+});
+
+// console.log(mockedGames.games[0]);
+let currentGameNumber = 0;
+
+firstMenu.submenu('Я просто смотрю', 'explore', new TelegrafInlineMenu(mockedGames.games[currentGameNumber].name, {
+  photo: mockedGames.games[currentGameNumber].pictureURL
+}))
+  .setCommand('explore')
+  .simpleButton('Назад', 'a', {
+    doFunc: async () => {
+      currentGameNumber--
+      console.log("nazad ", currentGameNumber)
+      // ctx.answerCbQuery('Just a callback query answer')
+    }
+  })
+  .simpleButton('В меню', 'b', {
+    doFunc: async ctx => ctx.answerCbQuery('Just a callback query answer'),
+    joinLastRow: true
+  })
+  .simpleButton('Вперёд', 'c', {
+    doFunc: async () => {
+      currentGameNumber++
+      console.log("vperyod ", currentGameNumber)
+      // ctx.answerCbQuery('Just a callback query answer')
+    },
+    joinLastRow: true
+  })
+
 
 bot.use(firstMenu.init());
 bot.startPolling();
---
+++ constants.js
@@ -0,0 +1,39 @@
+const TELEGRAM_BOT_KEY = "960038286:AAGZZHaFHCj1VCxvkBhGTLAYYWxd7DT7JNk"
+
+const mockedGames ={
+    games:[
+        {
+            id: "1",
+            name: "One",
+            pictureURL: "https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/999/EP4497-CUSA16579_00-00000000000000P1/1596558946000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000",
+        },
+        {
+            id: "2",
+            name: "Two",
+            pictureURL: "https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/999/EP0102-CUSA14123_00-BH3B000000000001/1592010505000/image?w=480&h=480&bg_color=000000&opacity=100&_version=00_09_000",
+        },
+        {
+            id: "3",
+            name: "Three",
+            pictureURL: "https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/999/EP0102-CUSA14123_00-BH3B000000000001/1592010505000/image?w=480&h=480&bg_color=000000&opacity=100&_version=00_09_000",
+        },
+        {
+            id: "4",
+            name: "Four",
+            pictureURL: "https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/999/EP0102-CUSA14123_00-BH3B000000000001/1592010505000/image?w=480&h=480&bg_color=000000&opacity=100&_version=00_09_000",
+        },
+        {
+            id: "5",
+            name: "Five",
+            pictureURL: "https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/999/EP0102-CUSA14123_00-BH3B000000000001/1592010505000/image?w=480&h=480&bg_color=000000&opacity=100&_version=00_09_000",
+        },
+        {
+            id: "6",
+            name: "Six",
+            pictureURL: "https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/999/EP0102-CUSA14123_00-BH3B000000000001/1592010505000/image?w=480&h=480&bg_color=000000&opacity=100&_version=00_09_000",
+        }
+    ]
+}
+
+module.exports.mockedGames = mockedGames;
+module.exports.TELEGRAM_BOT_KEY = TELEGRAM_BOT_KEY;
