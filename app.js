b23a298ddbbe6ab1f2133b98ee4c9a2b87ec6e4f
jonarizzz <jonarizzz@inbox.ru>
2020-08-30 05:21:13 +0300

    Still need to change the structure of response (need to pass text as well as photo and buttons)

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import Telegraf, {Extra, Markup} from 'telegraf';
 import {TELEGRAM_BOT_KEY} from './constants/constants.js';
 import {exploreMenu, mainMenu} from "./keyboard/keyboard";
-import {greetingText} from "./constants/constants";
+import {greetingText, mockedGames} from "./constants/constants";
 import {firstMenu} from "./menu/menu";
 
 const bot = new Telegraf(TELEGRAM_BOT_KEY);
@@ -15,10 +15,8 @@ bot.start(ctx => ctx.reply(greetingText, mainMenu));
 
 // Сюда передать внешнюю переменную для перелистывания игр через кнопки
 bot.hears('🎮 Я просто смотрю', ctx => {
-  ctx.replyWithPhoto('https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/999/EP4497-CUSA165' +
-      '79_00-00000000000000P1/1596558946000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000')
-      .then(() =>   ctx.reply('Передать сюда текст (название игры)', Extra.markup(exploreMenu))
-  )
+  // ctx.reply('Передать сюда текст (название игры)', Extra.markup(exploreMenu))
+  ctx.replyWithPhoto({url: mockedGames.games[0].pictureURL}, Extra.markup(exploreMenu))
 });
 
 // bot.use(firstMenu.init());
